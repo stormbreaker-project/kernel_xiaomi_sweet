8e162f2297c4cbb33b8943ccd3aa960818a9ae23
MASTERGUY <murtipipo@gmail.com>
2021-05-09 21:59:16 +0530

    Add drone specific YML
    Also add a builds script & nook circleci deps while we are here
    
    Signed-off-by: Forenche <prahul2003@gmail.com>
    Signed-off-by: MASTERGUY <murtipipo@gmail.com>

---
+++ kramel.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+echo "Cloning dependencies"
+git clone --depth=1 https://github.com/stormbreaker-project/kernel_xiaomi_sweet -b V1 kernel
+cd kernel
+git clone --depth=1 -b master https://github.com/MASTERGUY/proton-clang clang
+git clone --depth=1 https://github.com/stormbreaker-project/AnyKernel3 -b sweet AnyKernel
+echo "Done"
+TANGGAL=$(date +"%F-%S")
+START=$(date +"%s")
+export CONFIG_PATH=$PWD/arch/arm64/configs/sweet_user_defconfig
+PATH="${PWD}/clang/bin:$PATH"
+export ARCH=arm64
+export KBUILD_BUILD_HOST="Drone-CI"
+export KBUILD_BUILD_USER="mtpiplod"
+# Send info plox channel
+function sendinfo() {
+    curl -s -X POST "https://api.telegram.org/bot$BOTTOKEN/sendMessage" \
+        -d chat_id="$CHATID" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=html" \
+        -d text="<b>• Sweet-Stormbreaker Kernel •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Redmi Note 10 Pro/Max</b> (sweet/sweetin)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>Proton clang 13</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b> #AOSP-Alpha"
+}
+# Push kernel to channel
+function push() {
+    cd AnyKernel
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot$BOTTOKEN/sendDocument" \
+        -F chat_id="$CHATID" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Redmi Note 10 Pro/Max (sweet)</b>"
+}
+# Fin Error
+function finerr() {
+    curl -F document=@$LOG "https://api.telegram.org/bot$BOTTOKEN/sendDocument" \
+        -F chat_id="$CHATID" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Build logs"
+}
+# Compile plox
+function compile() {
+ make sweet_user_defconfig O=out
+    make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                      NM=llvm-nm \
+                      OBJCOPY=llvm-objcopy \
+                      OBJDUMP=llvm-objdump \
+                      STRIP=llvm-strip
+  cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+}
+# Zipping
+function zipping() {
+    cd AnyKernel || exit 1
+    zip -r9 Sweet-StormBreaker-${TANGGAL}.zip *
+    cd ..
+}
+sendinfo
+compile
+zipping
+END=$(date +"%s")
+DIFF=$(($END - $START))
+finerr
+push
